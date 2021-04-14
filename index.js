518fa797e383c6fee8bfb51e765d9674fba4c73d
tristantheb <tristantheb@gmail.com>
2021-04-14 14:52:11 +0200

    Improve the javascript file

--- index.js
+++ index.js
@@ -2,100 +2,106 @@
 const lang = 'fr';
 let original = [], localized = [];
 
+const $htmlElm = {};
+
 // Start operation when the DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
-  // Get the tables
-  const $changesTable = document.querySelector('table.changes-table > tbody');
-  const $quickNav = document.querySelector('nav.quick-nav > ul');
-  
-  async function fecthAllFiles() {
-    // Fetch the origin version
-    console.info("Start fetching original file");
-    await fetch('history/logs-en-us.txt')
-      .then(response => response.text())
-      .then(text => {
-        let regex = /^(.*\.html)$/gm;
-        let res = text.match(regex)
-        original = original.concat(res);
-      })
-      .catch((e) => {
-        console.error(`An error occurred while loading the original file: ${e.message}`)
-      });
-      
-    // Fetch the translated version
-    console.info("Start fetching translated file");
-    await fetch(`history/logs-${lang}.txt`)
-      .then(response => response.text())
-      .then(text => {
-        let regex = /^(.*\.html)$/gm;
-        localized = localized.concat(text.match(regex));
-      })
-      .then(() => {
-        for (let i = 0; i < localized.length; i++) {
-          let str = localized[i];
-          if (str.includes("/conflicting/") || str.includes("/orphaned/")) {
-            localized.splice(i, 1);
-            i--;
-          }
-        }
-      })
-      .catch((e) => {
-        console.error(`An error occurred while loading the original file: ${e.message}`);
-      });
+  $htmlElm.changesTable = document.querySelector('table.changes-table > tbody');
+  $htmlElm.quickNav = document.querySelector('nav.quick-nav > ul');
 
-    generateTables();
-    generateNav();
+  fecthAllFiles()
+    .then(() => {
+        // When all is ok, generate table and navigation
+        generateTables();
+        generateNav();
+    })
+    .catch(e => {
+      console.error('There has been a problem with your fetch operation: ' + e.message);
+    });
+});
+
+async function fecthAllFiles() {
+  // Fetch the origin version
+  let response = await fetch('history/logs-en-us.txt');
+
+  if (response.ok) {
+    let txt = await response.text();
+    let regex = /^(.*\.html)$/gm;
+    let res = txt.match(regex);
+    original = original.concat(res);
+  } else {
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
+    
+  // Fetch the translated version
+  response = await fetch(`history/logs-${lang}.txt`);
 
-  /**
-   * This function takes care of generating the two tables to see the
-   * differences and missing pages from the DND Web Docs
-   */
-  async function generateTables() {
-    console.info("Start generating table");
-    // Remove missing pages from origin var and move to missing var
-    let html = '';
+  if (response.ok) {
+    let text = await response.text();
+    let regex = /^(.*\.html)$/gm;
+    localized = localized.concat(text.match(regex));
 
-    original.forEach((el, i) => {
-      let date = el.match(/^(.*) /g);
-      let pathName = el.match(/(files\/.*)$/g);
+    for (let i = 0; i < localized.length; i++) {
+      let str = localized[i];
+      if (str.includes("/conflicting/") || str.includes("/orphaned/")) {
+        localized.splice(i, 1);
+        i--;
+      }
+    }
+  } else {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+}
 
-      let pathShort = pathName[0].slice(11);
+/**
+ * This function takes care of generating the table to see the
+ * differences and missing pages from the MDN Web Docs
+ */
+function generateTables() {
+  // Remove missing pages from origin var and move to missing var
+  let html = '';
 
-      let dateLoca, pathNameLoca;
-      localized.forEach(l10n => {
-        if (l10n.includes(pathShort)) {
-          dateLoca = l10n.match(/^(.*) /g);
-          pathNameLoca = l10n.match(/(files\/.*)$/g);
-        }
-      });
+  original.forEach((el, i) => {
+    let date = el.match(/^(.*) /g);
+    let pathName = el.match(/(files\/.*)$/g);
 
-      let date1, date2, color;
-      date1 = new Date(date);
-      date2 = dateLoca ? new Date(dateLoca) : undefined;
-      color = date1 < date2 ? "success" :
-              date2 === undefined ? "error" :
-              "warning";
+    // Remove the en-US for searching the existing pages
+    let pathShort = pathName[0].slice(11);
 
-      if (!pathNameLoca && !dateLoca) {
-        html += `<tr id="${i}" class="${color}"><th scope="row">#${i}</th><td>${pathName}</td><td class="m-hide">${date}</td><td colspan="2"><div class="note missing"><p><b>Warning !</b></p><p>This page is missing.</p></div></td></tr>`;
-      } else {
-        html += `<tr id="${i}" class="${color}"><th scope="row">#${i}</th><td>${pathName}</td><td class="m-hide">${date}</td><td>${pathNameLoca}</td><td class="m-hide">${dateLoca}</td></tr>`;
+    let dateLoca, pathNameLoca;
+    localized.forEach(l10n => {
+      if (l10n.includes(pathShort)) {
+        dateLoca = l10n.match(/^(.*) /g);
+        pathNameLoca = l10n.match(/(files\/.*)$/g);
       }
     });
 
-    $changesTable.innerHTML = html;
-  }
+    let date1, date2, color;
+    date1 = new Date(date);
+    date2 = dateLoca ? new Date(dateLoca) : undefined;
+    color = date1 < date2 ? "success" :
+            date2 === undefined ? "error" :
+            "warning";
 
-  async function generateNav() {
-    console.info("Start generating navigation");
-    let count = (original.length - (original.length % 100)) / 100, html = '';
-    for (let i = 1; i <= count; i++) {
-      html += `<li><a href="#${i * 100}">#${i * 100}</a></li>`;
+    if (!pathNameLoca && !dateLoca) {
+      html += `<tr id="${i}" class="${color}"><th scope="row">#${i}</th><td>${pathName}</td><td class="m-hide">${date}</td><td colspan="2"><div class="note missing"><p><b>Warning !</b></p><p>This page is missing.</p></div></td></tr>`;
+    } else {
+      html += `<tr id="${i}" class="${color}"><th scope="row">#${i}</th><td>${pathName}</td><td class="m-hide">${date}</td><td>${pathNameLoca}</td><td class="m-hide">${dateLoca}</td></tr>`;
     }
+  });
 
-    $quickNav.innerHTML = html;
+  $htmlElm.changesTable.innerHTML = html;
+}
+
+/**
+ * This function takes care of the number pages from the MDN Web Docs
+ * and generate on page links to all 100 pages
+ */
+function generateNav() {
+  let count = (original.length - (original.length % 100)) / 100, html = '';
+  for (let i = 1; i <= count; i++) {
+    html += `<li><a href="#${i * 100}">#${i * 100}</a></li>`;
   }
 
-  fecthAllFiles();
-});
+  $htmlElm.quickNav.innerHTML = html;
+}
