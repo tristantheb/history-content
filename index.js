fec7641d5b7550c85d5f486cdd8781ca4353876f
tristantheb <tristantheb@gmail.com>
2022-03-25 22:19:38 +0100

    Design update for l10n-fr
    Squashed commit of the following:
    
    commit a68801b232482efb53b04a029da2bc18484e7e2f
    Author: tristantheb <tristantheb@gmail.com>
    Date:   Fri Mar 25 22:18:08 2022 +0100
    
        Updating design to dark version
    
        Squashed commit of the following:
    
        commit 57e7f5be12e7a3a7d2be80355f5cde68c24e4452
        Author: tristantheb <tristantheb@gmail.com>
        Date:   Fri Mar 25 22:16:07 2022 +0100
    
            Improve some elements
    
        commit 9a46d5cf91961a564b6bfe0401e56bdd5aeacbb5
        Author: tristantheb <tristantheb@gmail.com>
        Date:   Fri Mar 25 21:59:12 2022 +0100
    
            Status as icons
    
        commit 8bf538c2c4777b228295e477620a001f139cbdb8
        Author: tristantheb <tristantheb@gmail.com>
        Date:   Fri Mar 25 21:14:18 2022 +0100
    
            Changing design for dark version

--- index.js
+++ index.js
@@ -6,70 +6,70 @@ const $htmlElm = {};
 
 // Start operation when the DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
-  $htmlElm.main = document.querySelector('.main-page-content');
-  $htmlElm.changesTable = document.querySelector('table.changes-table');
-  $htmlElm.changesTableContent = document.querySelector('table.changes-table > tbody');
-  $htmlElm.quickNav = document.querySelector('nav.quick-nav > ul');
+    $htmlElm.changesTable = document.querySelector('#changes-table');
+    $htmlElm.changesTableContent = document.querySelector('#changes-table > tbody');
+    $htmlElm.quickNav = document.querySelector('nav > ul');
 
-  fecthAllFiles()
-    .then(() => {
-        // When all is ok, generate table and navigation
-        generateTables();
-        generateNav();
-    })
-    .then(() => {
-      let counts = {}
-      counts.success = document.querySelectorAll('tr.success').length;
-      counts.warning = document.querySelectorAll('tr.warning').length;
-      counts.error = document.querySelectorAll('tr.error').length;
-      counts.max = counts.success + counts.warning + counts.error;
+    fecthAllFiles()
+        .then(() => {
+            // When all is ok, generate table and navigation
+            generateTables();
+            generateNav();
+        })
+        .then(() => {
+            let counts = {}
+            counts.success = document.querySelectorAll('tr.updated').length;
+            counts.warning = document.querySelectorAll('tr.outdated').length;
+            counts.error = document.querySelectorAll('tr.missing').length;
+            counts.max = counts.success + counts.warning + counts.error;
 
-      const $newDiv = document.createElement('div');
-      
-      $newDiv.innerHTML = `<div class="progress">
-        <div style="background-color: var(--green-200);width: ${counts.success / counts.max * 100}%;">${counts.success}</div>
-        <div style="background-color: var(--yellow-200);width: ${counts.warning / counts.max * 100}%;">${counts.warning}</div>
-        <div style="background-color: var(--red-200);width: ${counts.error / counts.max * 100}%;">${counts.error}</div>
-      </div>`;
+            const $newDiv = document.createElement('div');
+            
+            $newDiv.innerHTML = `<h3 id="some_statistics" class="text-lg text-slate-100 md:text-xl xl:text-2xl mb-2">Some statistics</h3>
+            <div class="flex my-4 rounded ring-1 ring-inset ring-white/10 shadow-lg overflow-hidden">
+                <div class="bg-green-900/30 text-green-300 p-2" style="width: ${counts.success / counts.max * 100}%;">${counts.success}</div>
+                <div class="bg-yellow-900/30 text-yellow-300 p-2" style="width: ${counts.warning / counts.max * 100}%;">${counts.warning}</div>
+                <div class="bg-red-900/30 text-red-200 p-2" style="width: ${counts.error / counts.max * 100}%;">${counts.error}</div>
+            </div>`;
 
-      $htmlElm.main.insertBefore($newDiv, $htmlElm.changesTable);
-    })
-    .catch(e => {
-      console.error('There has been a problem with your fetch operation: ' + e.message);
-    });
+            $htmlElm.changesTable.parentElement.insertBefore($newDiv, $htmlElm.changesTable);
+        })
+        .catch(e => {
+            console.error('There has been a problem with your fetch operation: ' + e.message);
+        });
 });
 
 async function fecthAllFiles() {
-  // Fetch the origin version
-  let response = await fetch('history/logs-en-us.txt');
+    // Fetch the origin version
+    let response = await fetch('history/logs-en-us.txt');
 
-  if (response.ok) {
-    let txt = await response.text();
-    let regex = /^(.*\.md)$/gm;
-    let res = txt.match(regex);
-    original = original.concat(res);
-  } else {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-    
-  // Fetch the translated version
-  response = await fetch(`history/logs-${lang}.txt`);
-
-  if (response.ok) {
-    let text = await response.text();
-    let regex = /^(.*\.md)$/gm;
-    localized = localized.concat(text.match(regex));
-
-    for (let i = 0; i < localized.length; i++) {
-      let str = localized[i];
-      if (str.includes("/conflicting/") || str.includes("/orphaned/")) {
-        localized.splice(i, 1);
-        i--;
-      }
+    if (response.ok) {
+        let txt = await response.text();
+        let regex = /^(.*\.md)$/gm;
+        let res = txt.match(regex);
+        original = original.concat(res);
+    } else {
+        throw new Error(`HTTP error! status: ${response.status}`);
     }
-  } else {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
+        
+    // Fetch the translated version
+    response = await fetch(`history/logs-${lang}.txt`);
+
+    if (response.ok) {
+        let text = await response.text();
+        let regex = /^(.*\.md)$/gm;
+        localized = localized.concat(text.match(regex));
+
+        for (let i = 0; i < localized.length; i++) {
+            let str = localized[i];
+            if (str.includes("/conflicting/") || str.includes("/orphaned/")) {
+                localized.splice(i, 1);
+                i--;
+            }
+        }
+    } else {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
 }
 
 /**
@@ -77,40 +77,43 @@ async function fecthAllFiles() {
  * differences and missing pages from the MDN Web Docs
  */
 function generateTables() {
-  // Remove missing pages from origin var and move to missing var
-  let html = '';
+    // Remove missing pages from origin var and move to missing var
+    let html = '';
 
-  original.forEach((el, i) => {
-    let date = el.match(/^(.*) /g);
-    let pathName = el.match(/(files\/.*)$/g);
+    original.forEach((el, i) => {
+        let date = el.match(/^(.*) /g);
+        let pathName = el.match(/(files\/.*)$/g);
 
-    // Remove the en-US for searching the existing pages
-    let pathShort = pathName[0].slice(11);
+        // Remove the en-US for searching the existing pages
+        let pathShort = pathName[0].slice(11);
 
-    let dateLoca, pathNameLoca;
-    localized.forEach(l10n => {
-      if (l10n.includes(pathShort)) {
-        dateLoca = l10n.match(/^(.*) /g);
-        pathNameLoca = l10n.match(/(files\/.*)$/g);
-      }
+        let dateLoca, pathNameLoca;
+        localized.forEach(l10n => {
+            if (l10n.includes(pathShort)) {
+                dateLoca = l10n.match(/^(.*) /g);
+                pathNameLoca = l10n.match(/(files\/.*)$/g);
+            }
+        });
+
+        let date1, date2, color, status;
+        date1 = new Date(date);
+        date2 = dateLoca ? new Date(dateLoca) : undefined;
+        color = date1 < date2 ? "updated bg-green-900/30 text-green-300" :
+                date2 === undefined ? "missing bg-red-900/30 text-red-200" :
+                "outdated bg-yellow-900/30 text-yellow-300";
+        status = date1 < date2 ? '<div class="bg-green-900/60 text-green-300 border-4 border-double border-green-300 text-center inline-flex p-1 rounded-full"><svg role="img" viewBox="0 0 31 21" fill="none" stroke="currentColor" stroke-width="6" stroke-linecap="round" class="inline h-5 w-5 p-0.5"><path d="M3 8l10 10 15-15"></path></svg><span class="sr-only">Up-to-date</span></div>' :
+                date2 === undefined ? '<div class="bg-red-900/60 text-red-300 border-4 border-double border-red-300 text-center inline-flex p-1 rounded-full"><svg role="img" viewBox="0 0 30 30" fill="none" stroke="currentColor" stroke-width="6" stroke-linecap="round" class="inline h-5 w-5 p-0.5"><path d="M3 3l24 24m-24 0 24-24"></path></svg><span class="sr-only">Missing</span></div>' :
+                '<div class="bg-yellow-900/60 text-yellow-300 border-4 border-double border-yellow-300 text-center inline-flex p-1 rounded-full"><svg role="img" viewBox="0 0 28 29" fill="none" stroke="currentColor" stroke-width="6" stroke-linecap="round" class="inline h-5 w-5 p-0.5"><path d="M14 3v15m0 8z"></path></svg><span class="sr-only">Outdated</span></div>';
+        
+        let id = i + 1;
+        if (!pathNameLoca && !dateLoca) {
+            html += `<tr id="${id}" class="${color} text-sm"><th scope="row" class="px-3 py-2">#${id}</th><td class="px-3 py-2">${pathName}</td><td></td><td class="hidden xl:block"></td><td class="px-3 py-2 text-center">${status}</td></tr>`;
+        } else {
+            html += `<tr id="${id}" class="${color} text-sm"><th scope="row" class="px-3 py-2">#${id}</th><td class="px-3 py-2">${pathName}</td><td class="px-3 py-2">${pathNameLoca}</td><td class="hidden xl:block px-3 py-2">${dateLoca}</td><td class="px-3 py-2 text-center">${status}</td></tr>`;
+        }
     });
 
-    let date1, date2, color;
-    date1 = new Date(date);
-    date2 = dateLoca ? new Date(dateLoca) : undefined;
-    color = date1 < date2 ? "success" :
-            date2 === undefined ? "error" :
-            "warning";
-    
-    let id = i + 1;
-    if (!pathNameLoca && !dateLoca) {
-      html += `<tr id="${id}" class="${color}"><th scope="row">#${id}</th><td>${pathName}</td><td class="m-hide">${date}</td><td colspan="2"><div class="note missing"><p><b>Missing page !</b></p><p>This page is only available in English, you might want to translate it.</p></div></td></tr>`;
-    } else {
-      html += `<tr id="${id}" class="${color}"><th scope="row">#${id}</th><td>${pathName}</td><td class="m-hide">${date}</td><td>${pathNameLoca}</td><td class="m-hide">${dateLoca}</td></tr>`;
-    }
-  });
-
-  $htmlElm.changesTableContent.innerHTML = html;
+    $htmlElm.changesTableContent.innerHTML = html;
 }
 
 /**
@@ -118,11 +121,11 @@ function generateTables() {
  * and generate on page links to all 100 pages
  */
 function generateNav() {
-  let count = (original.length - (original.length % 100)) / 100, html = '';
-  for (let i = 1; i <= count; i++) {
-    let id = i * 100;
-    html += `<li><a href="#${id}">#${id}</a></li>`;
-  }
+    let count = (original.length - (original.length % 100)) / 100, html = '';
+    for (let i = 1; i <= count; i++) {
+        let id = i * 100;
+        html += `<li><a href="#${id}" class="text-blue-300 hover:text-blue-50 visited:text-purple-500">#${id}</a></li>`;
+    }
 
-  $htmlElm.quickNav.innerHTML = html;
+    $htmlElm.quickNav.innerHTML = html;
 }
