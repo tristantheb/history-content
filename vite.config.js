28e7b6b62128d31402e0128e5fda9002c3ec91f9
Tristan D. <tristantheb@users.noreply.github.com>
2025-09-18 03:36:53 +0200

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/history-content/',
-  content_root: '../content/',
-  translated_root: '../translated-content/',
+  base: '/',
 })
