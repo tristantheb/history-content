46b8309e1d656f368765ac58a5123e8dddead0c7
Tristan D. <tristantheb@users.noreply.github.com>
2026-02-19 22:13:46 +0100

    chore(design): removed old css on Pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -10,37 +10,21 @@ type PaginationProps = {
 
 const Pagination = ({ page, totalPages, setPage, customClass = '' }: PaginationProps) => {
   return (
-    <div className={`flex gap-2 items-center justify-center md:justify-end ${customClass}`}>
+    <div className={`pagination ${customClass}`}>
       <button
         onClick={() => setPage(p => Math.max(1, p - 1))}
         disabled={page === 1}
         aria-label={'Previous page'}
-        className={
-          'p-2 rounded-full bg-zinc-700 hover:bg-slate-600 text-white ' +
-          'font-semibold transition disabled:opacity-50 hover:cursor-pointer ' +
-          'disabled:cursor-not-allowed focus:outline-none focus:ring-2 focus:ring-blue-400'
-        }
       >
         <ChevronLeft color={'currentColor'} />
       </button>
-      <span className={'dark:text-zinc-200 font-medium text-base px-2 select-none'}>
-        Page
-        {' '}
-        <span className={'text-blue-700 dark:text-blue-300 font-bold'}>{page}</span>
-        {' '}
-        /
-        {' '}
-        <span className={'text-blue-950 dark:text-blue-200'}>{totalPages || 1}</span>
+      <span>
+        Page {page} / {totalPages || 1}
       </span>
       <button
         onClick={() => setPage(p => Math.min(totalPages, p + 1))}
         disabled={page === totalPages || totalPages === 0}
         aria-label={'Next page'}
-        className={
-          'p-2 rounded-full bg-zinc-700 hover:bg-slate-600 text-white ' +
-          'font-semibold transition disabled:opacity-50 hover:cursor-pointer ' +
-          'disabled:cursor-not-allowed focus:outline-none focus:ring-2 focus:ring-blue-400'
-        }
       >
         <ChevronRight color={'currentColor'} />
       </button>
