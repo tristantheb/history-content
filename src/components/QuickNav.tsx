64cbc4d3a2b146a9bf822a49c1866390f746e93f
Tristan D. <tristantheb@users.noreply.github.com>
2026-02-19 23:43:01 +0100

    chore(design): removed old css on QuickNav

--- src/components/QuickNav.tsx
+++ src/components/QuickNav.tsx
@@ -4,20 +4,19 @@ import { UntranslatedHash } from '@/components/StatusIcons/UntranslatedHash'
 import { UpToDateHash } from '@/components/StatusIcons/UpToDateHash'
 
 const QuickNav = () => (
-  <aside className={'lg:order-1 lg:col-span-1'}>
-    <h2 id={'quick_navigation'} className={'font-semibold text-xl md:text-2xl xl:text-3xl mb-3'}>
+  <aside className={'navigation-aside'}>
+    <h2 id={'quick_navigation'}>
       Quick navigation
     </h2>
-    <h3 id={'mdn_repositories'} className={'text-lg md:text-xl xl:text-2xl mb-2'}>
+    <h3 id={'mdn_repositories'}>
       MDN Repositories
     </h3>
-    <ul className={'mb-2'}>
+    <ul>
       <li>
         <a
           href={'https://github.com/mdn/content'}
           target={'_blank'}
-          rel={'noreferrer'}
-          className={'text-blue-300 hover:text-blue-500 dark:hover:text-blue-50 visited:text-purple-500'}>
+          rel={'noreferrer'}>
           GitHub mdn/content
         </a>
       </li>
@@ -25,28 +24,31 @@ const QuickNav = () => (
         <a
           href={'https://github.com/mdn/translated-content'}
           target={'_blank'}
-          rel={'noreferrer'}
-          className={'text-blue-300 hover:text-blue-500 dark:hover:text-blue-50 visited:text-purple-500'}>
+          rel={'noreferrer'}>
           GitHub mdn/translated-content
         </a>
       </li>
     </ul>
-    <h2 id={'icons-explanation'} className={'font-semibold text-xl md:text-2xl xl:text-3xl mb-3'}>
+    <h2 id={'icons-explanation'}>
       Icons explanation
     </h2>
-    <p className={'mb-2'}>This is described as follows:</p>
-    <ul className={'mb-2 pl-4'}>
-      <li className={'py-1'}>
-        <UpToDateHash />&nbsp;The document use the latest hash commit of the original version.
+    <p>This is described as follows:</p>
+    <ul>
+      <li>
+        <span className={'text-green'}><UpToDateHash size={20} /></span>&nbsp;
+        The document use the latest hash commit of the original version.
       </li>
-      <li className={'py-1'}>
-        <OutdatedHash />&nbsp;The document need to be updated to the latest hash commit of the original version.
+      <li>
+        <span className={'text-red'}><OutdatedHash size={20} /></span>&nbsp;
+        The document need to be updated to the latest hash commit of the original version.
       </li>
-      <li className={'py-1'}>
-        <MissingHash />&nbsp;The document doesn't have a hash commit.
+      <li>
+        <span className={'text-yellow'}><MissingHash size={20} /></span>&nbsp;
+        The document doesn't have a hash commit.
       </li>
-      <li className={'py-1'}>
-        <UntranslatedHash />&nbsp;The document has not been translated into your language.
+      <li>
+        <span className={'text-gray'}><UntranslatedHash size={20} /></span>&nbsp;
+        The document has not been translated into your language.
       </li>
     </ul>
   </aside>
